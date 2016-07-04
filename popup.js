80547b260e6488ead5e972c398d92a14c731cd5d
nikhilv2007 <nikhilv_2007@yahoo.co.in>
2016-07-04 23:58:56 +0530

    delete bookmark
    added support to delete individual bookmark

--- popup.js
+++ popup.js
@@ -30,7 +30,7 @@ function getBookmarks(bookmarkFolder){
 				
 	}
 	else if(bookmarkFolder.url != undefined){
-		popupContent += "<li><input type='checkbox' data-id='"+bookmarkFolder.id+"' value='"+bookmarkFolder.url+"'/><a target='_blank' href='"+ bookmarkFolder.url +"' title='" +bookmarkFolder.url+ "'>" +bookmarkFolder.title+ "</a></li>";
+		popupContent += "<li data-id='"+bookmarkFolder.id+"'><input type='checkbox' value='"+bookmarkFolder.url+"'/><a target='_blank' href='"+ bookmarkFolder.url +"' title='" +bookmarkFolder.url+ "'>" +bookmarkFolder.title+ "</a><a title='Delete'><img src='images/delete.png'/></a></li>";
 	}
 }
 
@@ -59,18 +59,25 @@ function openBookmarks(){
             chrome.windows.create({url: checkedURLs, state: "maximized", incognito: true});
             break;
     }
+    
+    // Deselect checkboxes
+    deselectBookmarks();
 }
 
 function deleteBookmarks(){
     if (confirm("Are you sure deleting bookmark(s)")){
         var inputElements = document.getElementsByTagName('input');
         for(var i=0; i< inputElements.length; i++){
-            if(inputElements[i].checked)
-                chrome.bookmarks.remove(inputElements[i].getAttribute('data-id'));
+            if(inputElements[i].checked){
+                deleteBookmark(inputElements[i].parentElement.getAttribute('data-id'));
+                inputElements[i].parentElement.parentElement.removeChild(inputElements[i].parentElement);
+            }
         }
-        // Refresh page
-        location.reload();
     }
+}
+
+function deleteBookmark(bookmarkId){
+    chrome.bookmarks.remove(bookmarkId);
 }
 
 function deselectBookmarks(){
@@ -85,6 +92,25 @@ function editBookmark(){
     // Edit name, URL values
 }
 
+function handleClick(event) {
+    event = event || window.event;
+    event.target = event.target || event.srcElement;
+
+    var element = event.target;
+
+    // Climb up the document tree from the target of the event
+    while (element) {
+        if (element.nodeName === "A" && /Delete/.test(element.getAttribute('title'))) {
+            // The user clicked on a <a> or clicked on an element inside <a> with title "Delete"
+            deleteBookmark(element.parentElement.getAttribute('data-id'));
+            element.parentElement.parentElement.removeChild(element.parentElement);
+            break;
+        }
+
+        element = element.parentNode;
+    }
+}
+
 window.addEventListener('load', function(evt) {
 
    	document.getElementById('btnOpenBookmarks').addEventListener('click', openBookmarks);
@@ -93,4 +119,5 @@ window.addEventListener('load', function(evt) {
     
     document.getElementById('btnDeselectBookmarks').addEventListener('click', deselectBookmarks);
    	
+    document.addEventListener("click", handleClick, false);
 });
